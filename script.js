12f0d3937f0c373a6d10541c538a9235d5774d38
Juwon <dnladesanya1@gmail.com>
2022-09-28 17:28:21 +0100

    made changes to script.js

--- script.js
+++ script.js
@@ -1,55 +1,30 @@
 /* eslint-disable no-use-before-define */
-
-const title = document.querySelector('.title'); //title input
-const author = document.querySelector('.author'); //author input
-const form = document.querySelector('.form'); //form
-const bookAdd = document.querySelector('.list'); 
-let books = [];
-
-
-function updateUI() {
-  bookAdd.innerHTML = '';
-  books.forEach((data, index) => {
-    const classBook = document.createElement('div');
-    classBook.classList.add('class-book');
-    const par = document.createElement('p');
-    par.textContent = `${data.title} By ${data.author}`;
-    const btnRemove = document.createElement('button');
-    btnRemove.textContent = 'Remove';
-    btnRemove.addEventListener('click', removeBook.bind(index));
-    classBook.appendChild(par);
-    classBook.appendChild(btnRemove);
-    bookAdd.appendChild(classBook);
-  });
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (title.value && author.value) {
-    const obj = { title: title.value, author: author.value };
-    books.push(obj);
-    saveBooks(books);
-    displayBookData();
-    title.value = '';
-    author.value = '';
+ class Book {
+  constructor ( title, author) {
+    this.title = title
+    this.author =author
   }
-});
+ }
 
-function saveBooks(book) {
-    localStorage.setItem('bookInfo', JSON.stringify(book));
-  }
-if (!localStorage.getItem('bookInfo')) {
-  localStorage.setItem('bookInfo', JSON.stringify([]));
-}
+ //class to add a book to the book array
+ 
 
-  function displayBookData() {
-    books = JSON.parse(localStorage.getItem('bookInfo'));
-    updateUI();
-  }
-  function removeBook() {
-    books.splice(this, 1);
-    saveBooks(books);
-    displayBookData();
-  }
 
-displayBookData();
+
+
+
+
+ // add event to submit button
+
+const formSumbit = document.querySelector('.form');
+formSumbit.addEventListener('submit', function (e){
+
+  //prevet default submit action
+   e.preventDefault()
+
+  //attach the form inputs
+  const titleinput = document.querySelector('.title').value;
+  const authorinput = document.querySelector('.author').value;
+
+  const books = new Book (titleinput, authorinput)
+}) 
