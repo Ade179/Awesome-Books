867f1d7e1ef0701a6fd0f01ce0400772b40fb8d8
Juwon <dnladesanya1@gmail.com>
2022-09-29 10:48:38 +0100

    updated js

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 /* eslint-disable no-use-before-define */
- class Book {
-  constructor ( title, author) {
+class Book {
+  constructor(title, author) {
     this.title = title
-    this.author =author
+    this.author = author
   }
- }
+  //Update the user interface
+  updateUI() {
+    bookAdd.innerHTML = '';
+    bookslist.forEach((data, index) => {
+      const classBook = document.createElement('div');
+      classBook.classList.add('class-book');
+      const par = document.createElement('p');
+      par.textContent = `${data.title} By ${data.author}`;
+      const btnRemove = document.createElement('button');
+      btnRemove.textContent = 'Remove';
+      classBook.appendChild(par);
+      classBook.appendChild(btnRemove);
+      bookAdd.appendChild(classBook);
+    });
+  }
+}
 
- //create bookslist array
- const bookslist = []
+const bookAdd = document.querySelector('.list')
+
+//create bookslist array
+const bookslist = []
 console.log(bookslist)
 
-//Update the user interface
 
 
-
-
-
- // add event to submit button
+// add event to submit button
 
 const formSumbit = document.querySelector('.form');
-formSumbit.addEventListener('submit', function (e){
+formSumbit.addEventListener('submit', function (e) {
 
   //prevet default submit action
-   e.preventDefault()
+  e.preventDefault()
 
   //attach the form inputs
   const titleinput = document.querySelector('.title').value;
   const authorinput = document.querySelector('.author').value;
 
-  const books = new Book (titleinput, authorinput)
+  const books = new Book(titleinput, authorinput)
   bookslist.push(books)
-  
+
 
 
 }) 
