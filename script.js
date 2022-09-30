93dbeea66fb1fdebab6eccb757f29501503cddbb
Juwon <dnladesanya1@gmail.com>
2022-09-30 07:49:59 +0100

    linters fixed

--- script.js
+++ script.js
@@ -41,8 +41,10 @@ class Book {
     this.#save();
   }
 }
+
 const awesomeBooks = new Book();
 awesomeBooks.load();
+
 function updateUI() {
   bookAdd.innerHTML = awesomeBooks.getBooks().map((book) => `
               <li class="container">
@@ -52,7 +54,6 @@ function updateUI() {
              
           `).join('');
 }
-
 updateUI();
 
 // display error
@@ -61,7 +62,7 @@ function removeError() {
   error.style.display = 'none';
 }
 
-const formSumbit = document.querySelector('.form');
+const formSumbit = document.querySelector('.add-book');
 
 formSumbit.addEventListener('submit', (e) => {
   const title = document.querySelector('.title').value;
@@ -86,4 +87,41 @@ bookAdd.addEventListener('click', (event) => {
     awesomeBooks.deleteBook(+id);
   }
   updateUI();
+});
+
+const dateSection = document.querySelector('.date');
+const dateOptions = {
+  time: 'numeric',
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+
+};
+
+const blah = new Date().toLocaleDateString('en-US', dateOptions);
+dateSection.innerHTML = blah;
+
+const bookList = document.querySelector('.list-link');
+const addNew = document.querySelector('.add-new');
+const contact = document.querySelector('.contact');
+
+const bookSection = document.querySelector('.books-list');
+const addNewSection = document.querySelector('.form');
+const contactSection = document.querySelector('.contact-container');
+bookSection.style.display = 'none';
+bookList.addEventListener('click', () => {
+  bookSection.style.display = 'block';
+  addNewSection.style.display = 'none';
+  contactSection.style.display = 'none';
+});
+addNew.addEventListener('click', () => {
+  bookSection.style.display = 'none';
+  addNewSection.style.display = 'block';
+  contactSection.style.display = 'none';
+});
+contact.addEventListener('click', () => {
+  bookSection.style.display = 'none';
+  addNewSection.style.display = 'none';
+  contactSection.style.display = 'block';
 });
